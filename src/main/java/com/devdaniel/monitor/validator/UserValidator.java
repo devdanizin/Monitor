90e0ad42380d2088af63f1cd1538cc7cb681e3bd
devdanizin <devdanielprof@gmail.com>
2025-07-02 18:57:52 -0300

    UserValidator is a component

--- src/main/java/com/devdaniel/monitor/validator/UserValidator.java
+++ src/main/java/com/devdaniel/monitor/validator/UserValidator.java
@@ -1,10 +1,12 @@
 package com.devdaniel.monitor.validator;
 
 import com.devdaniel.monitor.model.User;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
+@Component
 public class UserValidator {
 
     public boolean isUserExpired(User user) {
