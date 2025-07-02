7d205f4b9a57aed5c359ad5e603a726ce8443473
devdanizin <devdanielprof@gmail.com>
2025-07-02 10:52:57 -0300

    Ajusting

--- src/main/java/com/devdaniel/monitor/configuration/SecurityConfig.java
+++ src/main/java/com/devdaniel/monitor/configuration/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/admin.html", "/api/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .userDetailsService(userDetailsService)
